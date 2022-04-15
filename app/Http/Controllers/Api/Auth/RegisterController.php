cffe4d20f815e07a923994395672211eab4e40aa
KomangSurya267 <komangsurya267@gmail.com>
2022-04-15 11:06:07 +0700

    api register

---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $request -> validate([
+            'name' => 'required',
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:8']
+        ]);
+        
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+          ]);
+
+        return UserResource::make($user)->additional([
+            'token' => $user->createToken('seal-api')->plainTextToken,
+        ]);
+    }
+}
